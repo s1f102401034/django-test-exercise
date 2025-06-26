843d58058ef9458fbf09e395ceeb298267292616
1F10240103 <s1f102401034@iniad.org>
2025-06-26 12:39:42 +0900

    テストを追加

--- todo/views.py
+++ todo/views.py
@@ -13,6 +13,6 @@ def index(request):
     tasks = Task.objects.all()
 
     context = {
-        'task' : tasks
+        'tasks' : tasks
     }
     return render(request, 'todo/index.html', context)
